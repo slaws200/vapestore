697be46f5ba2b0042d42a1e74f917f48fb784e24
slaws <den200xx@gmail.com>
2025-01-13 18:05:22 +0300

    add check in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 import { useEffect } from 'react';
 
 const App = () => {
-  useEffect(() => {
-    const tg = window.Telegram.WebApp;
-    tg.ready();
-  }, []);
+    useEffect(() => {
+        if (window.Telegram?.WebApp) {
+            const tg = window.Telegram.WebApp;
+            tg.ready(); // Уведомляем Telegram, что приложение готово
+        } else {
+            console.log("Запущено вне Telegram");
+        }
+    }, []);
 
   return <div>Welcome to the store!</div>;
 };
