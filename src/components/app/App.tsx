3feb8a03f65c0416e419709061857666a2cb813b
slaws <den200xx@gmail.com>
2025-01-14 17:18:12 +0300

    -

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,6 +1,5 @@
 
 import { useEffect } from 'react';
-
 import React, { useState } from 'react';
 import Header from '@components/Header/Header';
 import ProductList from '@components/ProductList/ProductList';
@@ -8,13 +7,13 @@ import BottomMenu from '@components/BottomMenu/BottomMenu';
 import './app.css';
 
 const mockProducts = [
-  { id: '1', name: 'Angry Wape', image: '/img/product1.jpeg', price: '$10' },
-  { id: '2', name: 'Jewel Juice', image: '/img/product2.png', price: '$20' },
-  { id: '3', name: 'Island Man', image: '/img/product3.jpg', price: '$15' },
-  { id: '4', name: 'Pawn Juice', image: '/img/product4.jpg', price: '$25' },
-  { id: '5', name: 'Pawn Juice', image: '/img/product4.jpg', price: '$25' },  
-  { id: '6', name: 'Pawn Juice', image: '/img/product4.jpg', price: '$25' },
-  { id: '7', name: 'Pawn Juice', image: '/img/product4.jpg', price: '$25' },
+  { id: '1', name: 'Angry Wape', image: 'img/product1.jpeg', price: '$100' },
+  { id: '2', name: 'Jewel Juice', image: 'img/product2.png', price: '$20' },
+  { id: '3', name: 'Island Man', image: 'img/product3.jpg', price: '$155' },
+  { id: '4', name: 'Pawn Juice', image: 'img/product4.jpg', price: '$65' },
+  { id: '5', name: 'Pawn Juice', image: 'img/product4.jpg', price: '$525' },  
+  { id: '6', name: 'Pawn Juice', image: 'img/product4.jpg', price: '$215' },
+  { id: '7', name: 'Pawn Juice', image: 'img/product4.jpg', price: '$80' },
 ];
 
 const App: React.FC = () => {
