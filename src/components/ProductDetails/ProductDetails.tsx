c8f15763adf3d9aab55fbdbb852f1f48c68ace82
Your Name <you@example.com>
2025-01-17 00:24:38 +0200

    -

--- src/components/ProductDetails/ProductDetails.tsx
+++ src/components/ProductDetails/ProductDetails.tsx
@@ -21,18 +21,21 @@ const ProductDetails: React.FC<ProductDetailsProps> = ({ products, baseQuantity,
 
   return (
     <div className="product-details">
-      <div className="close" onClick={() => navigate("/vapestore/")}>&#215;</div>
+      <div className="close" onClick={() => {
+        navigate("/vapestore/");
+        onQuantityChange(1);
+        }}>&#215;</div>
       <img src={'/vapestore/' + product.image} alt={product.name} className="product-details-image" />
       <h1 className="product-details-name">{product.name}</h1>
       <p className="product-details-price">Цена: {product.price}</p>
       <div className="product-buttons-wrapper">
         <button className="product-dec-button" onClick={() => {
           onQuantityChange(baseQuantity === 1 ? 1 : baseQuantity - 1)
-        }}>&#8722;</button>
+        }}>-</button>
         <div className="product-quantity">{baseQuantity}</div>
         <button className="product-inc-button" onClick={() => {
           onQuantityChange(baseQuantity === 10 ? 10 : baseQuantity + 1)
-        }}>&#43;</button>
+        }}>+</button>
       </div>
       <p className="product-details-description">{product.description}</p>
       <button className="product-add-to-cart">Добавить в корзину</button>
