663363ad793f5a5547330e63f4a2ea45eb204ebf
slaws <den200xx@gmail.com>
2025-01-22 17:21:54 +0300

    add conditions to auto deploy

--- server/main.js
+++ server/main.js
@@ -1,34 +1,19 @@
-const TelegramBot = require('node-telegram-bot-api');
+const { Bot } = require("grammy");
 require("dotenv").config();
 
-const bot = new TelegramBot(process.env.API_KEY_BOT, {
+const bot = new Bot(process.env.API_KEY_BOT, {
 
     polling: true
     
 });
 
-bot.on('text', async msg => {
+bot.command("start", (ctx) => 
+  ctx.reply(`Приветствую${ctx.chat.username ? ', @' + ctx.chat.username : ', ' + ctx.chat.first_name}! Ты попал в магазин жидкостей для вейпа LIQUID  LOUNGE 😎, наша официальная группа - https://t.me/LiquidLoungevk, подписывайся и следи за обновлениями!`)
+);
 
-    console.log(msg);
-    try {
+bot.on("message", (ctx) => {
+  ctx.reply(`Здесь ты можешь посмотреть весь наш ассортимент премиум-жидкостей для вейпа и под систем💨\n[Смотреть ассортимент](https://t.me/LiquidLoungeBot/LiquidLounge)`, { parse_mode: 'Markdown', disable_web_page_preview: true});
+});
 
-        if(msg.text == '/start') {
-
-            await bot.sendMessage(msg.chat.id, `Приветствую, ${msg.chat.first_name}! Ты попал в магазин жидкостей для вейпа LIQUID LOUNGE 😎, наша официальная группа - https://t.me/LiquidLoungevk, подписывайся и следи за обновлениями!`);
-
-        }
-        else {
-
-            await bot.sendMessage(msg.chat.id, `Здесь ты можешь посмотреть весь наш ассортимент премиум-жидкостей для вейпа и под систем💨\n[Смотреть ассортимент](https://t.me/LiquidLoungeBot/LiquidLounge)`, { parse_mode: 'Markdown', disable_web_page_preview: true});
-
-        }
-
-    }
-    catch(error) {
-
-        console.log(error);
-
-    }
-
-})
+bot.start();
 
